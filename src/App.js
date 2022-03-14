bc6e9a303fa67bb238bd2c0b95eaa4cf11bbeab2
Martin Nirtl <martin@nirtl.rocks>
2022-03-14 16:15:24 +0100

    migrate to CRA

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import './App.css'
+import classNames from 'classnames';
+import { useState } from 'react'
+import { useLocalStorage } from './lib/hooks';
+import { generateReport } from './lib/dynatrace';
+
+export default function Index() {
+  const [tenant, setTenant] = useLocalStorage('dt_tenant', process.env.REACT_APP_DT_TENANT || 'https://xxxxxxxx.live.dynatrace.com');
+  const [token, setToken] = useLocalStorage('dt_token', process.env.REACT_APP_DT_TOKEN || '');
+  const [isLoading, setLoadingState] = useState(false);
+  const [file, setFile] = useState(null);
+  const [message, setMessage] = useState()
+
+  const updateTenant = (e) => {
+    e.preventDefault();
+    setTenant(e.target.value);
+  }
+
+  const updateToken = (e) => {
+    e.preventDefault();
+    setToken(e.target.value);
+  }
+
+  const generateReportAction = async () => {
+    if (!tenant || !token) {
+      setMessage('Please configure tenant and token!')
+
+      return
+    } else {
+      setMessage(undefined)
+    }
+
+    setLoadingState(true);
+    const report = await generateReport(tenant, token);
+    const fileBlob = new Blob([report])
+
+    const fileUrl = URL.createObjectURL(fileBlob)
+
+    setFile(fileUrl);
+    setLoadingState(false);
+  }
+
+  return (
+    <div className="App is-flex is-justify-content-center is-align-items-center">
+      <div className="p-5" style={{ width: '800px' }}>
+        <h1 className="title">AppSec Reporter</h1>
+        <h2 className="subtitle">Generate security reports based on Dynatrace Application Security</h2>
+        <p className="">
+          Dynatrace{' '}
+          <a href="https://www.dynatrace.com/support/help/how-to-use-dynatrace/application-security">
+            Application Security
+          </a>{' '}
+          enables you to detect, visualize, analyze, monitor, and remediate open-source and third-party vulnerabilities
+          in production and pre-production environments at runtime.
+        </p>
+        <p className="is-size-6 has-text-weight-semibold mt-4">
+          The generated CSV-file will use <span className="has-text-weight-bold">;</span> as delimiter and requires the following token scopes:
+        </p>
+        <div className="tags are-small mt-1">
+          <p className="tag is-info is-light">Read security problems</p>
+          <p className="tag is-info is-light">Read entities</p>
+        </div>
+        <div className="mt-4">
+          <div className="field">
+            {/* <label className="label">Dynatrace Tenant</label> */}
+            <div className="control">
+              <input className="input is-small" type="text" placeholder="Dynatrace Tenant" onChange={updateTenant} value={tenant} />
+            </div>
+          </div>
+          <div className="field">
+            {/* <label className="label">Dynatrace Token</label> */}
+            <div className="control">
+              <input className="input is-small" type="password" placeholder="Dynatrace Token" onChange={updateToken} value={token} />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className={classNames('button', 'is-info', 'is-fullwidth', { 'is-loading': isLoading })} onClick={generateReportAction}>
+                {file ? 'Regenerate' : 'Generate CSV Report'}
+              </button>
+            </div>
+            {message && <p className="help is-danger">{message}</p>}
+          </div>
+          {file &&
+            <div className="field">
+              <div className="control">
+                <a className={classNames('button', 'is-success', 'is-fullwidth')} href={file} download={`appsec-report_${new Date().toISOString().slice(0, 10)}.csv`}>
+                  Download CSV Report
+                </a>
+              </div>
+            </div>
+          }
+          <p className="help">
+            Need help? Reach out to <a href="mailto:martin.nirtl@dynatrace.com">martin.nirtl@dynatrace.com</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/dynatrace.js
@@ -0,0 +1,141 @@
+import { Parser } from 'json2csv';
+
+const getHostId = (type, entity) => {
+  switch (type) {
+    case 'HOST':
+      return entity.entityId;
+
+    case 'KUBERNETES_NODE':
+      return entity.fromRelationships.isNodeOfHost[0].id;
+
+    case 'PROCESS_GROUP_INSTANCE':
+      return entity.fromRelationships.isProcessOf[0].id;
+
+    default:
+      return 'UNSUPPORTED ENTITY TYPE';
+  }
+}
+
+const extractEntityInformation = (entity) => {
+  const type = entity.entityId.split('-')[0]
+
+  return {
+    entityDisplayName: entity.displayName,
+    entityDetectedName: entity.properties.detectedName,
+    hostId: getHostId(type, entity),
+  }
+}
+
+export const getSecurityProblems = async (tenant, token) => {
+  const SECURITY_PROBLEMS_API = `${tenant || process.env.DT_TENANT}/api/v2/securityProblems?pageSize=500`;
+  const SECURITY_PROBLEMS_DETAILS_API = `${tenant || process.env.DT_TENANT}/api/v2/securityProblems/`;
+  const FIELDS = '%2BriskAssessment%2C%2BmanagementZones%2C%2Bdescription%2C%2Bevents%2C%2BvulnerableComponents%2C%2BaffectedEntities%2C%2BexposedEntities%2C%2BreachableDataAssets%2C%2BrelatedEntities%2C%2BrelatedContainerImages'
+  const ENTITY_DETAILS_API = `${tenant || process.env.DT_TENANT}/api/v2/entities/`;
+
+  const securityProblemsList = [];
+  let nextPageKey = '';
+  do {
+    const res = await fetch(nextPageKey ? `${SECURITY_PROBLEMS_API}&nextPageKey=${nextPageKey}` : SECURITY_PROBLEMS_API, { method: 'GET', headers: { 'Authorization': `Api-Token ${token || process.env.DT_TOKEN}` } });
+    const body = await res.json();
+
+    securityProblemsList.push(...body.securityProblems);
+
+    nextPageKey = body.nextPageKey;
+  } while (!!nextPageKey);
+
+  const detailedSecurityProblemsList = (await Promise.allSettled(securityProblemsList.map(
+    async ({ securityProblemId }) => {
+      const res = await fetch(SECURITY_PROBLEMS_DETAILS_API + securityProblemId + `?fields=${FIELDS}`, { method: 'GET', headers: { 'Authorization': `Api-Token ${token || process.env.DT_TOKEN}` } });
+      const p = await res.json();
+
+      return p;
+    }
+  ))).filter(e => e.status === 'fulfilled').map(e => e.value)
+
+  let idCount = 0;
+  const extendedSecurityProblemsList = detailedSecurityProblemsList.reduce((list, p, i) => {
+    const flattenedProblems = p.vulnerableComponents.reduce((entityList, component, j) => {
+      entityList.push(...component.affectedEntities.map(entity => ({
+        id: ++idCount,
+        displayId: p.displayId,
+        securityProblemId: p.securityProblemId,
+        entityId: entity,
+        title: p.title,
+        description: p.description,
+        type: p.vulnerabilityType,
+        technology: p.technology,
+        package: p.packageName,
+        component: component.displayName,
+        status: p.status,
+        riskLevel: p.riskAssessment.riskLevel,
+        riskScore: p.riskAssessment.riskScore,
+        exposed: p.riskAssessment.exposure !== "NOT_AVAILABLE" ? 'yes' : 'no',
+        dataAssets: p.riskAssessment.dataAssets !== "NOT_AVAILABLE" ? 'yes' : 'no',
+        publicExploit: p.riskAssessment.publicExploit !== "NOT_AVAILABLE" ? 'yes' : 'no',
+        muted: p.muted ? 'yes' : 'no',
+        internalLink: p.url,
+        // TODO change to array which holds link text and href
+        externalLink: p.externalVulnerabilityId?.startsWith('SNYK') ? `https://security.snyk.io/vuln/${p.externalVulnerabilityId}` : '-',
+        // cveIds: p.cveIds,
+        firstSeen: new Date(p.firstSeenTimestamp).toUTCString(),
+        lastUpdated: new Date(p.lastUpdatedTimestamp).toUTCString(),
+      })))
+
+      return entityList;
+    }, []);
+
+    list.push(...flattenedProblems)
+
+    return list
+  }, []);
+
+  return (await Promise.allSettled(extendedSecurityProblemsList.map(async p => {
+    const res = await fetch(ENTITY_DETAILS_API + p.entityId, { method: 'GET', headers: { 'Authorization': `Api-Token ${token || process.env.DT_TOKEN}` } })
+    const entity = await res.json();
+
+    return {
+      ...p,
+      ...extractEntityInformation(entity),
+    };
+  }))).filter(e => e.status === 'fulfilled').map(e => e.value)
+}
+
+export const generateReport = async (tenant, token) => {
+  const fields = [
+    { label: 'ID', value: 'id' },
+    { label: 'Dynatrace ID', value: 'displayId' },
+    { label: 'Security Problem ID', value: 'securityProblemId' },
+    { label: 'Entity ID', value: 'entityId' },
+    { label: 'Entity Name', value: 'entityDisplayName' },
+    { label: 'Title', value: 'title' },
+    // { label: 'Description', value: 'description' },
+    { label: 'Type', value: 'type' },
+    { label: 'Technology', value: 'technology' },
+    { label: 'Package', value: 'package' },
+    { label: 'Component', value: 'component' },
+    { label: 'Status', value: 'status' },
+    { label: 'Risk Level', value: 'riskLevel' },
+    { label: 'Risk Score', value: 'riskScore' },
+    { label: 'Exposed', value: 'exposed' },
+    { label: 'Data Assets', value: 'dataAssets' },
+    { label: 'Public Exploit', value: 'publicExploit' },
+    { label: 'Muted', value: 'muted' },
+    { label: 'First seen', value: 'firstSeen' },
+    { label: 'Last updated', value: 'lastUpdated' },
+    { label: 'Host ID', value: 'hostId' },
+    { label: 'Dynatrace Link', value: 'internalLink' },
+    { label: 'External Link', value: 'externalLink' },
+    { label: 'Entity Name (detected)', value: 'entityDetectedName' },
+  ]
+
+  const problems = await getSecurityProblems(tenant, token);
+
+  const customParser = new Parser({
+    fields,
+    defaultValue: '-',
+    delimiter: ';',
+    header: true,
+  });
+
+  return customParser.parse(problems);
+};
