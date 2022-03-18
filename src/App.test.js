617f2d60e312ace7d924fbc776cefb43f7449de1
Martin Nirtl <martin@nirtl.rocks>
2022-03-18 21:45:34 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const generateReportButton = screen.getByText('Generate CSV Report');
+  expect(generateReportButton).toBeInTheDocument();
 });
